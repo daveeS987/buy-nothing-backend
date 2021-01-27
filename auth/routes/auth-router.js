ea34223fa35dbc6751d9c1069b7dee6371865c01
Davee Sok <daveesok@gmail.com>
2021-01-27 11:55:17 -0800

    cleaned up extra console.logs

--- auth/routes/auth-router.js
+++ auth/routes/auth-router.js
@@ -24,23 +24,21 @@ router.post('/authZero', authZero, handleAuthZero);
 
 async function handleAuthZero(req, res, next) {
 
-  console.log('req.body', req.body);
-  console.log('req.token', req.token);
-  console.log('req.user', req.user);
-
   try {
-    let object = {
+    let output = {
       token: req.token,
       user: req.user,
     };
+
+    console.log('---------------------------------------------');
+    console.log('Handle AuthZero is sending this to front end:', output);
     res.set('auth', req.token);
-    res.status(200).json(object);
+    res.status(200).json(output);
   } catch(e) {
     next(e.message);
   }
 }
 
-// *** Remember to change line 27: This is temporary for development purposes
 async function handleSignUp(req, res, next) {
   try {
     let obj = {
