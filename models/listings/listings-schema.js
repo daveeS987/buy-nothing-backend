7caf1d7a5009a1e74f1f7b8a03e605c6be09a26f
Davee Sok <daveesok@gmail.com>
2021-04-13 16:33:07 -0500

    cleaned up some comments

--- models/listings/listings-schema.js
+++ models/listings/listings-schema.js
@@ -3,12 +3,6 @@
 const mongoose = require('mongoose');
 require('mongoose-schema-jsonschema')(mongoose);
 
-// const CommentSchema = mongoose.Schema({
-//   userName: { type: String, required:true },
-//   userId: { type: String, required:true },
-//   text: { type: String, required:true },
-// });
-
 const listings = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
@@ -23,20 +17,3 @@ const listings = mongoose.Schema({
 });
 
 module.exports = mongoose.model('listings', listings);
-
-//Arrays
-//Mongoose supports arrays of SchemaTypes and arrays of subdocuments.
-// Arrays of SchemaTypes are also called primitive arrays, and
-//arrays of subdocuments are also called document arrays.
-//const ToySchema = new Schema({ name: String });
-// const ToyBoxSchema = new Schema({
-//   toys: [ToySchema],
-//   buffers: [Buffer],
-//   strings: [String],
-//   numbers: [Number]
-//   // ... etc
-// });
-//example array schema at docs:/
-//https://mongoosejs.com/docs/schematypes.html#arrays
-
-//https://stackoverflow.com/questions/33049707/push-items-into-mongo-array-via-mongoose
